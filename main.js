168a9330d45be649e39796006853ed336c209364
alecvittori <160054583+alecvittori@users.noreply.github.com>
2024-04-24 12:42:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -41,3 +41,38 @@ const csvFilePath = 'data.csv';
 
 // Read data from CSV file and convert it to array of Item instances
 const items = csvToItems(csvFilePath);
+
+
+
+// Accordion; nav functions 
+function myAccFunc(targetId) {
+  var x = document.getElementById(targetId);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// Function to open the cart
+function openCart() {
+  // Display the cart by changing its display style to "block"
+  document.getElementById("cart").style.display = "block";
+}
+
+// Click on the "Jeans" link on page load to open the accordion for demo purposes
+document.getElementById("myBtn").click();
+
+
+// Open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+
